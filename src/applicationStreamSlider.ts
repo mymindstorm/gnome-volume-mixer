3e10813573a50df5ba3b51860e0336b0a2cb1241
Brendan Early <mymindstorm@evermiss.net>
2020-08-27 23:25:39 -0500

    * connect stream to volume slider * add label to slider

--- src/applicationStreamSlider.ts
+++ src/applicationStreamSlider.ts
@@ -1,13 +1,26 @@
 import { MixerSinkInput } from "../@types/Gjs/Gvc-1.0";
+import { BoxLayout, Label } from "../@types/Gjs/St-1.0";
 
 // https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/master/js/ui/status/volume.js
 const VolumeStatus = imports.ui.status.volume;
 
 export var ApplicationStreamSlider = class extends VolumeStatus.StreamSlider {
   constructor(stream: MixerSinkInput) {
-    super();
+    super(VolumeStatus.getMixerControl());
 
-    this._slider.accessible_name = stream.get_name();
+    this.stream = stream;
     this._icon.icon_name = stream.get_icon_name();
+
+    if (stream.get_name()) {
+      this._vbox = new BoxLayout({ vertical: true });
+
+      this._label = new Label({ text: stream.get_name() });
+      this._vbox.add(this._label);
+
+      this.item.remove_child(this._slider);
+      this._vbox.add(this._slider);
+
+      this.item.actor.add(this._vbox);
+    }
   }
 };
