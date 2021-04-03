8547768da64c1cab2090c5599b150f4ae6166afd
Brendan Early <mymindstorm@evermiss.net>
2021-04-03 12:08:00 -0500

    update ts-for-girrc file

--- .ts-for-girrc.js
+++ .ts-for-girrc.js
@@ -1,11 +1,21 @@
 module.exports = {
-  environments: ['gjs'],
-  modules: ['Gtk-3.0', 'Shell-0.1'],
-  prettify: true,
-  girDirectories: [
-    '/usr/share/gir-1.0',
-    '/usr/share/gnome-shell',
-    '/usr/lib64/mutter-6'
-  ],
-  outdir: './@types',
+    "environments": [
+        "gjs"
+    ],
+    "modules": [
+        "Gtk-4.0",
+        "St-1.0",
+        "Gvc-1.0"
+    ],
+    "prettify": true,
+    "girDirectories": [
+        "/usr/share/gir-1.0",
+        "/usr/share/gnome-shell",
+        "/usr/lib64/mutter-"
+    ],
+    "outdir": "./@types",
+    "ignore": [
+        "Gtk-3.0",
+        "Gdk-3.0"
+    ]
 }
